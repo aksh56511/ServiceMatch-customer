69896c172f8d6f66ac683a23d30f76fd3b9f33b6
Aditi G Amin <aditigamin@gmail.com>
2025-12-31 11:16:28 +0530

    added internationalization

---
+++ src/i18n.js
@@ -0,0 +1,20 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import translationEN from "./locales/en/translation.json";
+import translationKN from "./locales/kn/translation.json";
+
+const resources = {
+  en: { translation: translationEN },
+  kn: { translation: translationKN }
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: "en", // default language
+    fallbackLng: "en",
+    interpolation: { escapeValue: false }
+  });
+
+export default i18n;
